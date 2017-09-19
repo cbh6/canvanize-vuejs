de3a49fb421726db4991a4172c1887e5af67994e
ext2165 <cristian.botella.hurtado@everis.com>
2017-09-19 14:37:13 +0200

    detail layout

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>
-    <p>{{project}}</p>
-  </div>
+  <div class="ui container">
+    <div class='ui centered fluid card'>
+      <div class='content'>
+          <div class='header'>
+              <h3 class="left aligned">{{project.title}}</h3>
+          </div>
+
+          <div class="ui horizontal segments">
+              <div class="ui card segment">
+                <div class="header teal ui top attached label">
+                  Notas
+                </div>
+                <div class="content">
+                  <div class="description">
+                    Aquí van las notas
+                  </div>
+                </div>
+              </div>
+              <div class="card ui segment">
+                <div class="header blue ui top attached label">
+                  Agenda
+                </div>
+                <div class="content">
+                  <div class="description">
+                    Eventos
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -15,7 +44,7 @@ export default {
   methods: {
     getProject: function(id){
       var projects = JSON.parse(localStorage.getItem("projects"));
-      return projects.filter(function(data){
+      return projects.find(function(data){
         return data.id == id;
       });
 
